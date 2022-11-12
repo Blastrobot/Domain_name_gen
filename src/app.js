9e0154ce98e8494d4be6c0aaf4ad00e00e5900aa
Charlie <carlosjcv94@gmail.com>
2022-11-12 12:02:30 +0000

    i think its done

--- src/app.js
+++ src/app.js
@@ -5,7 +5,32 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+//
+
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let dom = [".com", ".es", ".net"];
+
+let domains = document.querySelector("#domains"); // esta variable capturo mi p en html
+let content = ""; // variable donde se guardan todos los dominios
+
+for (let a = 0; a < pronoun.length; a++) {
+  let proele = pronoun[a];
+
+  for (let b = 0; b < adj.length; b++) {
+    let adjele = adj[b];
+
+    for (let c = 0; c < noun.length; c++) {
+      let nounele = noun[c];
+
+      for (let d = 0; d < dom.length; d++) {
+        let domele = dom[d];
+
+        // console.log(`${proele}${adjele}${nounele}${domele}`);
+        content += `${proele}${adjele}${nounele}${domele}<br>`;
+      }
+    }
+  }
+}
+domains.innerHTML = content;
